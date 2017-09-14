c33ffec732111f5ddd7d45054238805575b604ee
DESKTOP-R5U9TIM\현주네 <alwn5190@gmail.com>
2017-09-15 02:13:18 +0900

    [추가] AbstractFactoryPattern

---
+++ Unity_Pattern/Assets/Scripts/AbstractFactoryPattern/Ex1/AbstractFactory.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AbstractFactoryPattern.ex1
+{
+    public abstract class AbstractFactory
+    {
+        public abstract AbstractProductA CreateProductA();
+        public abstract AbstractProductB CreateProductB();
+    }
+
+    public abstract class AbstractProductA
+    {
+    }
+
+    public abstract class AbstractProductB
+    {
+        public abstract void Interact(AbstractProductA a);
+    }
+
+    public class ConcreteFactory1 : AbstractFactory
+    {
+        public override AbstractProductA CreateProductA()
+        {
+            return new ProductA1();
+        }
+
+        public override AbstractProductB CreateProductB()
+        {
+            return new ProductB1();
+        }
+    }
+
+
+
+    public class ProductA1 : AbstractProductA
+    {
+
+    }
+
+    public class ProductB1 : AbstractProductB
+    {
+        public override void Interact(AbstractProductA a)
+        {
+            Debug.Log(this.GetType().Name + " interacts with " + a.GetType().Name);
+        }
+    }
+
+    //public class ConcreteFactory2 : AbstractFactory
+    //{
+    //    public override AbstractProductA CreateProductA()
+    //    {
+    //        return new ProductA2();
+    //    }
+
+    //    public override AbstractProductB CreateProductB()
+    //    {
+    //        return new ProductB2();
+    //    }
+    //}
+
+
+    //public class ProductA2 : AbstractProductA
+    //{
+
+    //}
+
+    //public class ProductB2 : AbstractProductB
+    //{
+    //    public override void Interact(AbstractProductA a)
+    //    {
+    //        Debug.Log(this.GetType().Name + " interacts with " + a.GetType().Name);
+    //    }
+    //}
+}
---
+++ Unity_Pattern/Assets/Scripts/AbstractFactoryPattern/Ex1/MainProgram.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AbstractFactoryPattern.ex1
+{
+    public class MainProgram : MonoBehaviour
+    {
+        void Start()
+        {
+            AbstractFactory f1 = new ConcreteFactory1();
+            Client c1 = new Client(f1);
+            c1.Run();
+
+            //AbstractFactory f2 = new ConcreteFactory2();
+            //Client c2 = new Client(f2);
+            //c2.Run();
+        }
+    }
+
+    public class Client
+    {
+        private AbstractProductA _A;
+        private AbstractProductB _B;
+
+        public Client(AbstractFactory factory)
+        {
+            _A = factory.CreateProductA();
+            _B = factory.CreateProductB();
+        }
+
+        public void Run()
+        {
+            _B.Interact(_A);
+        }
+    }
+
+}
