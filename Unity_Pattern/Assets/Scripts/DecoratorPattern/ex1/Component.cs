cd3d4e8183316a0d65e19392879b5c57ec8da7e3
hyunju <hyunju.choi@blueark.com>
2017-09-19 18:59:17 +0900

    [추가] Decorator Pattern

---
+++ Unity_Pattern/Assets/Scripts/DecoratorPattern/ex1/Component.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DecoratorPattern.ex1
+{
+    public abstract class Component
+    {
+        public abstract void Operation();
+    }
+
+    public class ConcreteComponent : Component
+    {
+        public override void Operation()
+        {
+            Debug.Log("ConcreteComponent.Operation()");
+        }
+    }
+
+    public abstract class Decorator : Component
+    {
+        protected Component component;
+
+        public void SetComponent(Component component)
+        {
+            this.component = component;
+        }
+
+        public override void Operation()
+        {
+            if (component != null)
+                component.Operation();
+        }
+    }
+
+    public class ConcreteDecoratorA : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+            Debug.Log("ConcreteDecoratorA.Operation()");
+        }
+    }
+
+    public class ConcreteDecoratorB : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+            Debug.Log("ConcreteDecoratorB.Operation()");
+        }
+    }
+}
---
+++ Unity_Pattern/Assets/Scripts/DecoratorPattern/ex1/MainProgram.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DecoratorPattern.ex1
+{
+    public class MainProgram : MonoBehaviour
+    {
+        void Start()
+        {
+            ConcreteComponent c = new ConcreteComponent();
+            ConcreteDecoratorA dA = new ConcreteDecoratorA();
+            ConcreteDecoratorB dB = new ConcreteDecoratorB();
+
+            dA.SetComponent(c);
+            dB.SetComponent(dA);
+            dB.Operation();
+        }
+    }
+
+}
