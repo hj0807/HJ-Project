7ad67104c9d51ba3ebbed6c7f35a8e17821a7c75
hyunju <hyunju.choi@blueark.com>
2017-09-20 18:56:32 +0900

    [add] composite pattern ex1

---
+++ Unity_Pattern/Assets/Scripts/CompositePattern/ex1/Component.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CompositePattern.ex1
+{
+    public abstract class Component
+    {
+        protected string name;
+
+        public Component(string name)
+        {
+            //Debug.Log("Component 생성자 : " + name);
+            this.name = name;
+        }
+
+        public abstract void Add(Component component);
+        public abstract void Remove(Component component);
+        public abstract void Display(int depth);
+    }
+
+    public class Leaf : Component
+    {
+        public Leaf(string name) : base(name)
+        {
+        }
+
+        public override void Add(Component component)
+        {
+            Debug.Log("Can't add to a leaf");
+        }
+
+        public override void Remove(Component component)
+        {
+            Debug.Log("Can't remove from a leaf");
+        }
+
+        public override void Display(int depth)
+        {
+            Debug.Log(new string('-', depth) + name);
+        }
+    }
+
+    public class Composite : Component
+    {
+        private List<Component> _children = new List<Component>();
+
+        public Composite(string name):base(name)
+        {
+            //Debug.Log("====Composite생성자 : " + name);
+            this.name = name;
+        }
+
+        public override void Add(Component component)
+        {
+            _children.Add(component);
+        }
+
+        public override void Remove(Component component)
+        {
+            _children.Remove(component);
+        }
+
+        public override void Display(int depth)
+        {
+            Debug.Log(new string('-', depth) + name);
+
+            for(int i=0; i<_children.Count; i++)
+            {
+                _children[i].Display(depth + 2);
+            }
+        }
+    }
+}
---
+++ Unity_Pattern/Assets/Scripts/CompositePattern/ex1/MainProgram.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CompositePattern.ex1
+{
+    public class MainProgram : MonoBehaviour
+    {
+        void Start()
+        {
+            Composite root = new Composite("root");
+            root.Add(new Leaf("Leaf A"));
+            root.Add(new Leaf("Leaf B"));
+
+            Composite composite = new Composite("Composite X");
+            composite.Add(new Leaf("Leaf XA"));
+            composite.Add(new Leaf("Leaf XB"));
+
+            root.Add(composite);
+            root.Add(new Leaf("Leaf C"));
+
+            //Add and Remove a leaf
+            Leaf leaf = new Leaf("Leaf D");
+            root.Add(leaf);
+            root.Remove(leaf);
+
+            root.Display(1);
+        }
+    }
+}
