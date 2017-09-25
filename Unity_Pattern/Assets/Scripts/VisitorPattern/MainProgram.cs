5de30606e231e6b9ab76bdf1ed9ac392d7d4d93e
hyunju <hyunju.choi@blueark.com>
2017-09-25 19:05:12 +0900

    [추가] Visitor 패턴 ex1

---
+++ Unity_Pattern/Assets/Scripts/VisitorPattern/MainProgram.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VisitorPattern.ex1
+{
+    public class MainProgram : MonoBehaviour
+    {
+        void Start()
+        {
+            CarElement car = new Car();
+
+            car.Accept(new CarElementPrintVisitor());
+            car.Accept(new CarElementDoVisitor());
+        }
+    }
+}
