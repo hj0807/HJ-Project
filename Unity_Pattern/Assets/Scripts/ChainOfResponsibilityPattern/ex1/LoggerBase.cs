183f8cf2d9cdec7e7fe361bcb7deeabdde3a23e0
hyunju <hyunju.choi@blueark.com>
2017-09-26 19:01:25 +0900

    add

---
+++ Unity_Pattern/Assets/Scripts/ChainOfResponsibilityPattern/ex1/LoggerBase.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//==== http://www.dofactory.com/net/chain-of-responsibility-design-pattern
+
+//ex1 https://en.wikipedia.org/wiki/Chain-of-responsibility_pattern
+//ex2 https://www.codeproject.com/Articles/41786/Chain-of-Responsibility-Design-Pattern
+
+namespace ChainOfResponsibilityPattern.ex1
+{
+    [Flags]
+    public enum LogLevel
+    {
+        None = 0,
+        Info = 1,
+        Debug = 2,
+        Warning = 4,
+        Error - 8,
+        FuctionalMessage = 16,
+        FuctionalError = 32,
+        All = 63
+    }
+
+    public abstract class LoggerBase
+    {
+        protected LogLevel logMask;
+
+        protected LoggerBase next;
+
+        public LoggerBase(LogLevel logMask)
+        {
+            this.logMask = logMask;
+        }
+
+        public LoggerBase SetNext(LoggerBase nextLogger)
+        {
+            next = nextLogger;
+
+            return nextLogger;
+        }
+
+        public void Message(string msg, LogLevel level)
+        {
+            if(logMask && level)
+            {
+
+            }
+        }
+    }
+}
---
+++ Unity_Pattern/Assets/Scripts/ChainOfResponsibilityPattern/ex1/MainProgram.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ChainOfResponsibilityPattern.ex1
+{
+    //c# Chain of Responsibility 구현
+    //http://gorakgarak.tistory.com/465
+    //http://objectbuilder.tistory.com/84
+
+    //c# Chain of Responsibility
+
+    public class MainProgram : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+    }
+}
